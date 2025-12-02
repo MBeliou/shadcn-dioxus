c8197bfc4d6dec4e6ffe56d1c30b737ed73f5d5b
MBeliou <maxime.bele@gmail.com>
2025-12-03 00:19:41 +0100

    implement card description component

---
+++ packages/ui/src/card/card_description.rs
@@ -0,0 +1,28 @@
+use dioxus::prelude::*;
+
+use crate::cn;
+
+#[derive(Clone, PartialEq, Props)]
+pub struct CardDescriptionProps {
+    #[props(into, default)]
+    pub class: String,
+
+    pub children: Element,
+
+    #[props(extends = GlobalAttributes)]
+    pub attributes: Vec<Attribute>,
+}
+
+#[component]
+pub fn CardDescription(props: CardDescriptionProps) -> Element {
+    rsx! {
+        div {
+            "data-slot": "card-description",
+            class: cn("text-muted-foreground text-sm", &props.class),
+            ..props.attributes,
+            {props.children}
+        }
+    }
+}
+
+    
