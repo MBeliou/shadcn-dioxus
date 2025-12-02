7c50541005fabf921529eb03451466ab90369049
MBeliou <maxime.bele@gmail.com>
2025-12-02 14:45:50 +0100

    implement spinner

--- packages/ui/src/spinner/spinner.rs
+++ packages/ui/src/spinner/spinner.rs
@@ -2,26 +2,16 @@ use dioxus::prelude::*;
 use lucide_dioxus::LoaderCircle;
 
 use crate::cn;
-/*
-<script lang="ts">
-    import { cn } from "$lib/utils.js";
-    import Loader2Icon from "@lucide/svelte/icons/loader-2";
-    import type { ComponentProps } from "svelte";
 
-    let { class: className, ...restProps }: ComponentProps<typeof Loader2Icon> = $props();
-</script>
-
-<Loader2Icon
-    role="status"
-    aria-label="Loading"
-    class={cn("size-4 animate-spin", className)}
-    {...restProps}
-/>
- */
 #[derive(Clone, PartialEq, Props)]
 pub struct SpinnerProps {
     #[props(into, default)]
     pub class: String,
+
+    #[props(default = 24)]
+    pub size: usize,
+    #[props(default = "currentColor".to_owned())]
+    pub color: String,
 
     #[props(extends = GlobalAttributes)]
     pub attributes: Vec<Attribute>,
@@ -31,7 +21,9 @@ pub struct SpinnerProps {
 pub fn Spinner(props: SpinnerProps) -> Element {
     rsx! {
         LoaderCircle {
-            class: cn("size-4 animate-spin", &props.class)
+            class: cn("size-4 animate-spin", &props.class),
+            color: props.color,
+            size: props.size
 
         }
     }
