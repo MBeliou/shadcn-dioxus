707c471560577bf1afed8d2fd576e52d003d4fdc
MBeliou <maxime.bele@gmail.com>
2025-12-02 21:54:34 +0100

    implement empty description component

---
+++ packages/ui/src/empty/empty_description.rs
@@ -0,0 +1,24 @@
+use dioxus::prelude::*;
+
+use crate::cn;
+
+#[derive(Clone, PartialEq, Props)]
+pub struct EmptyDescriptionProps {
+    #[props(into, default)]
+    pub class: String,
+
+    pub children: Element,
+
+    #[props(extends = GlobalAttributes)]
+    pub attributes: Vec<Attribute>,
+}
+
+#[component]
+pub fn EmptyDescription(props: EmptyDescriptionProps) -> Element {
+    rsx! {
+        div {
+            "data-slot": "empty-description",
+            class: cn("text-muted-foreground [&>a:hover]:text-primary text-sm/relaxed [&>a]:underline [&>a]:underline-offset-4", &props.class),
+        }
+    }
+}
--- packages/ui/src/empty/mod.rs
+++ packages/ui/src/empty/mod.rs
@@ -3,3 +3,9 @@ pub use empty::*;
 
 mod empty_header;
 pub use empty_header::*;
+
+mod empty_title;
+pub use empty_title::*;
+
+mod empty_description;
+pub use empty_description::*;
