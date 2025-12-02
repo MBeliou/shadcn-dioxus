65f43c20b593531ae2427bcf144f08725fa74bc6
MBeliou <maxime.bele@gmail.com>
2025-12-03 00:00:03 +0100

    add example button to landing

--- packages/web/src/views/home.rs
+++ packages/web/src/views/home.rs
@@ -13,7 +13,13 @@ pub fn Home() -> Element {
                 section { class: "theme-container",
                     div { class: "mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-6 2xl:gap-8",
                         div { class: "flex flex-col gap-6 *:[div]:w-full *:[div]:max-w-full",
-                            "Demo coming soon"
+                            div {
+                                class: "flex space-x-2",
+                                Button {
+                                    variant: ui::ButtonVariant::Ghost,
+                                    "Examples"
+                                }
+                            }
                             Item { variant: ui::ItemVariant::Outline,
                                 ItemContent {
                                     ItemTitle { "Two-factor authentication" }
