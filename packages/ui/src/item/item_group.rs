9926e41b7fa73d417b15d25a6495e0fce4e15475
MBeliou <maxime.bele@gmail.com>
2025-12-05 13:50:47 +0100

    allow item group component to accept children

--- packages/ui/src/item/item_group.rs
+++ packages/ui/src/item/item_group.rs
@@ -3,13 +3,16 @@ use dioxus::prelude::*;
 use crate::cn;
 
 #[component]
-pub fn ItemGroup(#[props(into, default)] class: String) -> Element {
+pub fn ItemGroup(
+    #[props(into, default)] class: String,
+    children: Element,
+) -> Element {
     rsx! {
         div {
             role: "list",
             "data-slot": "item-group",
-            class: cn("group/item-group flex flex-col", &class)
+            class: cn("group/item-group flex flex-col", &class),
+            {children}
         }
-
     }
 }
