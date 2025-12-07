51820d1a50bb1ef7a080f1d10eaa07d0a62b1fe0
MBeliou <maxime.bele@gmail.com>
2025-12-07 20:25:20 +0100

    implement field set

---
+++ packages/ui/src/field/field_set.rs
@@ -0,0 +1,28 @@
+use crate::cn;
+use dioxus::prelude::*;
+
+#[derive(Props, Clone, PartialEq)]
+pub struct FieldSetProps {
+    #[props(into, default)]
+    pub class: String,
+
+    pub children: Element,
+
+    #[props(extends = GlobalAttributes)]
+    pub attributes: Vec<Attribute>,
+}
+
+#[component]
+pub fn FieldSet(props: FieldSetProps) -> Element {
+    rsx! {
+        fieldset {
+            "data-slot": "field-set",
+            class: cn(
+                "flex flex-col gap-6 has-[>[data-slot=checkbox-group]]:gap-3 has-[>[data-slot=radio-group]]:gap-3",
+                &props.class
+            ),
+            ..props.attributes,
+            {props.children}
+        }
+    }
+}
--- packages/ui/src/field/mod.rs
+++ packages/ui/src/field/mod.rs
@@ -1,4 +1,6 @@
 mod field;
+mod field_set;
 mod field_title;
 pub use field::*;
+pub use field_set::*;
 pub use field_title::*;
