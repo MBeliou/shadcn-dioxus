837a998015ff99383c0970718f783b704a6ee6a8
MBeliou <maxime.bele@gmail.com>
2025-12-07 20:21:41 +0100

    implement field title component

---
+++ packages/ui/src/field/field_title.rs
@@ -0,0 +1,28 @@
+use crate::cn;
+use dioxus::prelude::*;
+
+#[derive(Props, Clone, PartialEq)]
+pub struct FieldTitleProps {
+    #[props(into, default)]
+    pub class: String,
+
+    pub children: Element,
+
+    #[props(extends = GlobalAttributes)]
+    pub attributes: Vec<Attribute>,
+}
+
+#[component]
+pub fn FieldTitle(props: FieldTitleProps) -> Element {
+    rsx! {
+        div {
+            "data-slot": "field-title",
+            class: cn(
+                "flex w-fit items-center gap-2 text-sm font-medium leading-snug group-data-[disabled=true]/field:opacity-50",
+                &props.class
+            ),
+            ..props.attributes,
+            {props.children}
+        }
+    }
+}
--- packages/ui/src/field/mod.rs
+++ packages/ui/src/field/mod.rs
@@ -1,2 +1,4 @@
 mod field;
+mod field_title;
 pub use field::*;
+pub use field_title::*;
