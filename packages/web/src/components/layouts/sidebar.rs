44bb7941831b8a771bf2c6a563498b45a6b3af41
MBeliou <maxime.bele@gmail.com>
2025-12-08 18:25:29 +0100

    fix overflow on docs sidebar layout on windows

--- packages/web/src/components/layouts/sidebar.rs
+++ packages/web/src/components/layouts/sidebar.rs
@@ -9,7 +9,7 @@ pub fn SidebarLayout() -> Element {
         _ => String::new(),
     };
     rsx! {
-        div { class: "flex flex-1 w-screen",
+        div { class: "flex flex-1 w-full",
             Sidebar { active_slug }
             div { class: "flex-1",
                 div { class: "w-full max-w-2xl mx-auto py-6 px-4 md:px-0 lg:py-8", Outlet::<Route> {} }
