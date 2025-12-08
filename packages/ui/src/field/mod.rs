8d7e051436ee2256358746f5ad446c8780ad1205
MBeliou <maxime.bele@gmail.com>
2025-12-08 10:10:48 +0100

    implement field description component

--- packages/ui/src/field/mod.rs
+++ packages/ui/src/field/mod.rs
@@ -1,4 +1,5 @@
 mod field;
+mod field_description;
 mod field_error;
 mod field_group;
 mod field_label;
@@ -7,6 +8,7 @@ mod field_separator;
 mod field_set;
 mod field_title;
 pub use field::*;
+pub use field_description::*;
 pub use field_error::*;
 pub use field_group::*;
 pub use field_label::*;
